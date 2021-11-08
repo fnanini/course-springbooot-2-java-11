96323d9c90cf6f5979378e920ef37ccd2bd3717b
Felipe Nanini Cavelagna <felipenanini@gmail.com>
2021-11-07 23:51:12 -0300

    Database, test profile, JPA

--- src/main/java/com/cursojavaspringJPA/course/entities/User.java
+++ src/main/java/com/cursojavaspringJPA/course/entities/User.java
@@ -2,9 +2,17 @@ package com.cursojavaspringJPA.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -15,7 +23,6 @@ public class User implements Serializable{
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
