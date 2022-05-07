b6942ef06cccd068fbb6069a24f2fc2c970c1088
Felipe Nanini Cavelagna <felipenanini@gmail.com>
2022-05-07 19:37:27 -0300

    User insert

--- src/main/java/com/cursojavaspringJPA/course/services/UserService.java
+++ src/main/java/com/cursojavaspringJPA/course/services/UserService.java
@@ -1,4 +1,4 @@
-package com.cursojavaspringJPA.course.services;
+	package com.cursojavaspringJPA.course.services;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,5 +22,9 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj  = repository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
 	}
 }
